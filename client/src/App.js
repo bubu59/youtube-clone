22e146969536e415c4cb5ba44e09a4a30922e84a
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-10-23 22:33:28 +0800

    updating video card component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,13 @@ import styled, { ThemeProvider } from "styled-components"
 import Menu from "./components/Menu.jsx";
 import Navbar from "./components/Navbar.jsx";
 import { darkTheme, lightTheme } from "./utils/Theme.js";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Video from "./pages/Video.jsx";
 
 const Container = styled.div`
   display: flex
@@ -13,7 +20,9 @@ const Main = styled.div`
   background-color: ${({ theme }) => theme.bg}
 `
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  padding: 22px 96px
+`
 
 function App() {
   const [darkMode, setDarkMode] = useState(true)
@@ -21,13 +30,22 @@ function App() {
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
-        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
-        <Main>
-          <Navbar />
-          <Wrapper>
-            Video Cards
-          </Wrapper>
-        </Main>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
       </Container>
     </ThemeProvider>
   );
