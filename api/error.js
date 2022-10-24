c03bc59976fab7385aef02d99b465fd977d9a6af
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-10-24 23:24:23 +0800

    auth route done and tested!!

---
+++ api/error.js
@@ -0,0 +1,1 @@
+export const createError = (status, message)
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,16 @@
 import express from "express"
-import { } from "../controllers/auth.js"
+import { signin, signup } from "../controllers/auth.js"
 
 const router = express.Router()
 
+//CREATE A USER 
+router.post("/signup", signup)
+
+
+//SIGN IN 
+router.post("/signin", signin)
+
+//GOOGLE AUTH 
+router.post("/google",)
 
 export default router
